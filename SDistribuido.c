f313baa6147266b55dd18c19e7ff22ee5f6907e0
luizhmalves <luizhmalves@hotmail.com>
2016-04-05 10:23:34 -0300

    sistema distrbuido

--- SDistribuido.c
+++ SDistribuido.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include "biblioteca/sDistribuido.h"
-#define TRUE 1
 void main(int argc, char *argv[]){
 	int opcao;
-	while (TRUE){
-		printf("Este computador eh: <1> Servidor <2> Cliente.\n");
-		scanf(%d,&opcao);
-		switch(opcao){
-			case 1:
-				servidor();
-				break;
-			case 2:
-				cliente();
-				break;
-			default:
-				printf("Opcao invalida!!!\n");
-		}
+	printf("Este computador eh: <1> Servidor <2> Cliente.\n");
+	scanf(%d,&opcao);
+	switch(opcao){
+		case 1:
+			servidor();
+		break;
+		case 2:
+			cliente();
+		break;
+		default:
+			printf("Opcao invalida!!!\n");
 	}
 }
--- servidor.c
+++ servidor.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "biblioteca/sDistribuido.h"
-
+#define TRUE 1
 void servidor(){
 	int cpMem = 90;
 	int cpCpu = 90;
