2f8df4269a2708c53336fc297bd1e774ca84cba3
luizhmalves <luizhmalves@hotmail.com>
2016-04-07 12:33:33 -0300

    Testada e funcionando com as funções auxiliares

--- converteStringInt.c
+++ converteStringInt.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include "biblioteca/sDistribuido.h"
-void converteStringInt(char arg1[]){
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void converteStringInt(char *mensagem, int tamanho){
+	
+	char bufCpu[2];
+	char bufMem[2];
+	char bufTempo[6];
+	int cpu,mem,tempo,indice;
+	int indice1 = 0;
+	int indice2= 0;
+	int indice3 = 0;
+	for(indice = 0; indice < tamanho; i++){
+		if((mensagem[i] != '#' || mensagem != '?') && i < 3){
+			bufCpu[indice1] = mensagem[i];
+			indice1++;
+		}else if ((mensagem[i] != '#' || mensagem[i] != '?') && (i > 3 && i < 8)){
+			bufMem[indice2] = mensagem[i];
+			indice2++;
+		}else if ((mensagem[i] != '#' || mensagem[i] != '?') && (i > 8 && i < tamanho)){
+			bufTempo[indice3] = mensagem[i];
+			indice3++;
+		}
+	}
+	cpu = atoi(bufCpu);
+	mem = atoi(bufMem);
+	tempo = atoi(bufTempo);
+}
+		
+	
 /*FazerDepois 
  * usar a função atoi();
  * tem que separar a string para converter
